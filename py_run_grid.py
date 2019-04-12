0140323a842ed6fec6335d603444f781776d512b
Edward Parkinson <e.j.parkinson@soton.ac.uk>
2019-04-12 15:52:21 +0100

    Made less awful to look at

--- py_run_grid.py
+++ py_run_grid.py
@@ -4,59 +4,71 @@
 import os
 import py_run
 import shutil
+import numpy as np
 import py_run_util
+from typing import List
 
 
-def create_grid(pf, parameter, new_values):
+def create_grid(pf: str, parameter: str, grid: List[str]) -> List[str]:
     """
+    The purpose of this function is to use a base parameter file and to create
+    directories containing parameter files with a different value to the given
+    parameter. These values are given in grid.
+
+    This function will only work with one parameter and grids of multiple
+    parameters cannot be created.
+
+    A backup of the original base pf is made just in case :-).
 
     Parameters
     ----------
-    pf
-        path to the original Python pf
+    pf              str
+                    Path to the base Python pf
+    parameter       str
+                    The name of the parameter for which a grid will be created
+    grid            List[str]
+                    The values of the parameter to make a grid with
 
     Returns
     -------
-
+    pfs             List[str]
+                    A list containing the file paths to the newly created pfs.
     """
-
-    pfs = []
 
     if pf.find(".pf") == -1:
         pf += ".pf"
 
     sl = 0
-    for i, l in enumerate(pf):
-        if l == "/":
-            sl = i
+    for i, letter in enumerate(pf):  # This iterates over the pf file path
+        if letter == "/":
+            sl = i  # This will find the index of the final /
     pl = pf.find(".pf")
-    root = pf[sl:pl]
+    root = pf[sl:pl]  # Now we can extract just the root name from the file path
     shutil.copyfile(pf, pf + ".bak")
-
-    print(root)
 
     with open(pf, "r") as f:
         pf_lines = f.readlines()
 
     base = []
-    for l in pf_lines:
-        if l[0] == "#" or l[0] == "\n" or l[0] == "\r":
+    for letter in pf_lines:
+        if letter[0] == "#" or letter[0] == "\n" or letter[0] == "\r":
             continue
-        line = l.replace("\n", "").split()
-        base.append([line[0], line[1]])
+        letter = letter.replace("\n", "").split()
+        base.append([letter[0], letter[1]])
 
-    npfs = len(new_values)
+    # This bit creates the grid of pfs
+    pfs = []
+    npfs = len(grid)
     for i in range(npfs):
         new_pf = base.copy()
-        for l in range(len(new_pf)):
-            if new_pf[l][0] == parameter:
-                new_pf[l][1] = new_values[i]
+        for letter in range(len(new_pf)):
+            if new_pf[letter][0] == parameter:
+                new_pf[letter][1] = grid[i]
         try:
-            os.mkdir(new_values[i])
-        except OSError:
-            pass
-        new = "{}/{}.pf".format(new_values[i], root)
-        print(new)
+            os.mkdir(grid[i])
+        except OSError:  # if the directory already exists an OS error is raised
+            pass  # don't care for logging
+        new = "{}/{}.pf".format(grid[i], root)
         pfs.append(new)
         with open(new, "w") as f:
             for par, val in new_pf:
@@ -65,11 +77,47 @@ def create_grid(pf, parameter, new_values):
     return pfs
 
 
-def run_grid(pfs):
+def run_grid() -> None:
+    """
+    Main controlling function of the script.
+
+    Returns
+    -------
+    None
     """
 
-    """
+    print("ENSURE THAT THE SCRIPT HAS BEEN EDITED APPROPRIATELY BEFORE RUNNING")
+    input("Press a button to continue...")
 
+    # This is the parameter which will be changed
+    root = "tde.pf"
+    parameter = "Wind.mdot(msol/yr)"
+
+    grid = []
+    tmp = np.linspace(0.01, 0.1, 10)
+    for i in range(len(tmp)):
+        grid.append("{:.4e}".format(2e-1 * tmp[i]))
+
+    print("Running grid of {} simulations:".format(len(grid)))
+    print("Parameter: {}".format(parameter))
+    print("Grid values: {}".format(grid))
+
+    pfs = create_grid(root, parameter, grid)
+
+    # Set up global variables for py_run modules
+    py_run.PY_VERSION = "py"
+    py_run.PY_FLAGS = "-p 2"
+    py_run.RUN_SIMS = True
+    py_run.RESUME_RUN = False
+    py_run.CHECK_CONVERGENCE = True
+    py_run.CLIM = 0.85
+    py_run.CREATE_PLOTS = True
+    py_run.TDE_PLOT = True
+    py_run.NOT_QUIET = True
+    py_run.VERBOSE = False
+    py_run.SPEC_OVERRIDE = True
+
+    # Finally make a call to run_python to run all of the parameter files
     nsims = len(pfs)
     mpi, ncores = py_run_util.get_num_procs()
     py_run.run_python_etc(pfs, nsims, mpi, ncores)
@@ -77,34 +125,5 @@ def run_grid(pfs):
     return
 
 
-import numpy as np  ## fuck uuuuuuuuuuuuuuuuuuuuuuuuuuu
-
-
-def main():
-    """
-
-    """
-
-    parameter = "Wind.mdot(msol/yr)"
-    values = []
-    baa = np.linspace(0.01, 0.1, 10)
-    for i in range(len(baa)):
-        values.append("{:.4e}".format(2e-1 * baa[i]))
-
-    print(values)
-
-    pfs = create_grid("tde.pf", parameter, values)
-
-    py_run.RUN_SIMS = True
-    py_run.CHECK_CONVERGENCE = True
-    py_run.CREATE_PLOTS = True
-    py_run.TDE_PLOT = True
-    py_run.PY_FLAGS = "-p 2"
-
-    run_grid(pfs)
-
-    return
-
-
 if __name__ == "__main__":
-    main()
+    run_grid()
