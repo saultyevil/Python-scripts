204abc2a05ecee699fccc1a442d8767ecfa8dfa3
Edward Parkinson <e.j.parkinson@soton.ac.uk>
2020-04-20 13:23:08 +0100

    Clean up spectrum plotting script (so I have a better idea of what's happening) and move directories to a different file to import

---
+++ tde_optical/path.py
@@ -0,0 +1,56 @@
+
+# BLACK HOLE MASS
+
+mbh_grid = [
+    "Mbh/1.0000e+06",
+    "Mbh/1.0000e+07",
+    "Mbh/1.0000e+08"
+]
+
+mbh_labels = [
+    r"M$_{BH}$ = 10$^6$ M$_{\odot}$",
+    r"M$_{BH}$ = 10$^7$ M$_{\odot}$",
+    r"M$_{BH}$ = 10$^8$ M$_{\odot}$",
+]
+
+# INNER RADIUS
+
+rmin_grid = [
+    "Rmin/5.0000e+00",
+    "Rmin/1.0000e+01",
+    "Rmin/1.5000e+01"
+]
+
+rmin_labels = [
+    r"R$_{min}$ = 5 R$_{ISCO}$",
+    r"R$_{min}$ = 10 R$_{ISCO}$",
+    r"R$_{min}$ = 15 R$_{ISCO}$",
+]
+
+# TERMINAL VELOCITY
+
+vinf_grid = [
+    "Vinf/1.0000e-01",
+    "Vinf/5.0000e-01",
+    "Vinf/8.0000e-01"
+]
+
+vinf_labels = [
+    r"V$_{\infty}$ = 0.1 V$_{esc}$",
+    r"V$_{\infty}$ = 0.5 V$_{esc}$",
+    r"V$_{\infty}$ = 0.8 V$_{esc}$"
+]
+
+# FIDUCIAL MODEL FOR EACH GRID
+
+best_lines_grid = [
+    "Mbh/1.0000e+07",
+    "Rmin/1.5000e+01",
+    "Vinf/1.0000e-01"
+]
+
+best_lines_labels = [
+    r"M$_{BH}$ = 10$^7$ M$_{\odot}$",
+    r"R$_{min}$ = 15 R$_{ISCO}$",
+    r"V$_{\infty}$ = 0.1 V$_{esc}$"
+]
